d79cb9b6d2c8eb08f89b7268a74b9ec47d146a63
viperluan <viperluan@gmail.com>
2024-08-20 19:28:11 -0300

    adiciona linters, builders e configurações do typescript

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+
+export default [
+  {
+    files: ['src/**/*.{js,mjs,cjs,ts}'],
+    ignores: ['dist/', 'build/', 'node_modules/', 'pg_data/'],
+  },
+  {
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+];
