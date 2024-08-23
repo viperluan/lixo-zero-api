4cbec3cbe731acecea77dc1e3e3b7ad04184c310
viperluan <viperluan@gmail.com>
2024-08-22 21:15:30 -0300

    exclui migrations antigas

--- prisma/migrations/20240421180218_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "categoria" (
-    "id" SERIAL NOT NULL,
-    "descricao" TEXT NOT NULL,
-
-    CONSTRAINT "categoria_pkey" PRIMARY KEY ("id")
-);
--- prisma/migrations/20240617004038_drop_user_alter_required/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "acao" DROP CONSTRAINT "acao_id_usuario_alteracao_fkey";
-
--- AddForeignKey
-ALTER TABLE "acao" ADD CONSTRAINT "acao_id_usuario_alteracao_fkey" FOREIGN KEY ("id_usuario_alteracao") REFERENCES "usuario"("id") ON DELETE SET NULL ON UPDATE CASCADE;
