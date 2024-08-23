081db288b0b336a77bebc4d26c62568462f323c6
viperluan <viperluan@gmail.com>
2024-08-22 22:14:59 -0300

    adiciona metodos get e criptografia da senha

--- src/domain/entities/Usuario.ts
+++ src/domain/entities/Usuario.ts
@@ -1,4 +1,5 @@
 import { v4 as gerarUuid } from 'uuid';
+import bcrypt from 'bcrypt';
 
 export type UsuarioProps = {
   id: string;
@@ -11,18 +12,51 @@ export type UsuarioProps = {
 };
 
 export default class Usuario {
-  private constructor(readonly props: UsuarioProps) {}
+  private constructor(private props: UsuarioProps) {}
 
-  public static criarNovoUsuario(novoUsuario: Omit<UsuarioProps, 'id' | 'status' | 'tipo'>) {
+  public static criarNovoUsuario(
+    novoUsuario: Omit<UsuarioProps, 'id' | 'status' | 'tipo'>
+  ): Usuario {
+    const senhaCriptografada = bcrypt.hashSync(novoUsuario.senha, 10);
+
     return new Usuario({
       id: gerarUuid(),
       ...novoUsuario,
+      senha: senhaCriptografada,
       status: true,
-      tipo: 0,
+      tipo: 1,
     });
   }
 
-  public static carregarUsuarioExistente(props: UsuarioProps) {
+  public static carregarUsuarioExistente(props: UsuarioProps): Usuario {
     return new Usuario(props);
+  }
+
+  public get id() {
+    return this.props.id;
+  }
+
+  public get nome() {
+    return this.props.nome;
+  }
+
+  public get email() {
+    return this.props.email;
+  }
+
+  public get senha() {
+    return this.props.senha;
+  }
+
+  public get cpf_cnpj() {
+    return this.props.cpf_cnpj;
+  }
+
+  public get status() {
+    return this.props.status;
+  }
+
+  public get tipo() {
+    return this.props.tipo;
   }
 }
