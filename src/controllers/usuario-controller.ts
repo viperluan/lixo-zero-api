22af60fe784afd2ae93936e1108e28148eb90e55
viperluan <viperluan@gmail.com>
2024-08-23 22:17:13 -0300

    atualiza métodos do controller de usuario

--- src/controllers/usuario-controller.ts
+++ src/controllers/usuario-controller.ts
@@ -1,9 +1,13 @@
+import { prisma } from '../package/prisma';
 import { Request, Response } from 'express';
-import UsuarioPrismaRepository from '../application/repositories/UsuarioPrismaRepository';
-import CriarUsuario from '../application/usecases/CriarUsuario';
-import AutenticarUsuario from '../application/usecases/AutenticarUsuario';
 
-const usuarioPrismaRepository = new UsuarioPrismaRepository();
+import UsuarioPrismaRepository from '../application/repositories/UsuarioPrismaRepository';
+import CriarUsuario from '../application/usecases/usuario/CriarUsuario';
+import AutenticarUsuario from '../application/usecases/usuario/AutenticarUsuario';
+import DeletarUsuario from '../application/usecases/usuario/DeletarUsuario';
+import ListarUsuarios from '../application/usecases/usuario/ListarUsuarios';
+
+const usuarioPrismaRepository = new UsuarioPrismaRepository(prisma);
 
 export async function criar(request: Request, response: Response) {
   try {
@@ -20,29 +24,36 @@ export async function criar(request: Request, response: Response) {
 
 export async function buscarTodos(request: Request, response: Response) {
   try {
-    const { page = 1, limit = 10 } = request.query;
+    const { page, limit } = request.query;
 
-    const pageNumber = parseInt(page);
-    const limitNumber = parseInt(limit);
+    const paginaAtualRequest = Number(page);
+    const limiteRequest = Number(limit);
 
-    const { users, totalUsers } = await usuarioRN.listarUsuarios(pageNumber, limitNumber);
+    const listarUsuarios = new ListarUsuarios(usuarioPrismaRepository);
 
-    const totalPages = Math.ceil(totalUsers / limitNumber);
+    const { usuarios, paginaAtual, totalDePaginas } = await listarUsuarios.executar({
+      paginaAtual: paginaAtualRequest,
+      limiteDeUsuariosPorPagina: limiteRequest,
+    });
 
     response.status(200).json({
-      users,
-      totalPages,
-      currentPage: pageNumber,
+      users: usuarios,
+      totalPages: totalDePaginas,
+      currentPage: paginaAtual,
     });
   } catch (error) {
-    response.status(500).json({ error: (error as Error).message });
+    response.status(400).json({ error: (error as Error).message });
   }
 }
 
 export async function remover(request: Request, response: Response) {
   try {
-    const usuarioResponse = await usuarioRN.deletarUsuarioPorId(request.params.id);
-    response.status(200).json(usuarioResponse);
+    const { id } = request.params;
+
+    const deletarUsuario = new DeletarUsuario(usuarioPrismaRepository);
+    await deletarUsuario.executar({ id });
+
+    response.status(200).end();
   } catch (error) {
     response.status(400).json({ error: (error as Error).message });
   }
@@ -53,7 +64,7 @@ export async function autenticar(request: Request, response: Response) {
     const { email, senha } = request.body;
 
     const autenticarUsuario = new AutenticarUsuario(usuarioPrismaRepository);
-    const usuario = await autenticarUsuario.executar(email, senha);
+    const usuario = await autenticarUsuario.executar({ email, senha });
 
     response.status(200).json(usuario);
   } catch (error) {
