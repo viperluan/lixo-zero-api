01de8a7afa47ca1e6b564700fae4f4e4d6b9e21d
viperluan <viperluan@gmail.com>
2024-08-24 13:36:26 -0300

    corrige rotas de patrocinio

--- src/routes/patrocinioRoutes.ts
+++ src/routes/patrocinioRoutes.ts
@@ -4,7 +4,7 @@ import * as patrocinioController from '../controllers/patrocinador-controller';
 
 const patrocinioRouter = Router();
 
-patrocinioRouter.post('/patrocinio', patrocinioController.create);
-patrocinioRouter.get('/patrocinio', patrocinioController.getAll);
+patrocinioRouter.post('/', patrocinioController.create);
+patrocinioRouter.get('/', patrocinioController.getAll);
 
 export default patrocinioRouter;
