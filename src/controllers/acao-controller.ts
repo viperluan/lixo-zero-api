9cc0c63186bc256bb2d4aa16a7201b7cacfede19
viperluan <viperluan@gmail.com>
2024-09-05 18:03:41 -0300

    atualiza métodos de criar e listar ações

--- src/controllers/acao-controller.ts
+++ src/controllers/acao-controller.ts
@@ -1,10 +1,15 @@
-import { PrismaClient } from '@prisma/client';
+import { prisma } from '../package/prisma';
+import { Request, Response } from 'express';
+import AcaoPrismaRepository from '../application/repositories/AcaoPrismaRepository';
+import CriarAcao from '../application/usecases/acao/CriarAcao';
+import ListarAcoes from '../application/usecases/acao/ListarAcoes';
 import { AcaoRN } from '../business/acaoRN';
 
-const prisma = new PrismaClient();
-const acaoRN = new AcaoRN(prisma);
+const acaoPrismaRepository = new AcaoPrismaRepository(prisma);
 
-export async function getAll(req, res) {
+const acaoRN = new AcaoRN();
+
+export async function getAll(request: Request, response: Response) {
   try {
     const {
       page = 1,
@@ -15,67 +20,77 @@ export async function getAll(req, res) {
       search,
       situacao,
       forma_realizacao_acao,
-    } = req.query;
+    } = request.query;
 
-    const pageNumber = parseInt(page);
-    const limitNumber = parseInt(limit);
+    const paginaAtual = Number(page);
+    const limiteDeAcoesPorPagina = Number(limit);
 
-    const filters = {
-      id_categoria,
-      id_usuario,
-      data_acao,
-      search,
-      situacao,
-      forma_realizacao_acao,
+    const filtros = {
+      id_categoria: (id_categoria as string) || '',
+      id_usuario: (id_usuario as string) || '',
+      data_acao: (data_acao as string) || '',
+      search: (search as string) || '',
+      situacao: (situacao as string) || '',
+      forma_realizacao_acao: (forma_realizacao_acao as string) || '',
     };
 
-    const { actions, totalActions } = await acaoRN.listarAcoes(filters, pageNumber, limitNumber);
+    const listarAcoes = new ListarAcoes(acaoPrismaRepository);
+    const { acoes, totalDePaginas } = await listarAcoes.executar({
+      filtros,
+      limiteDeAcoesPorPagina,
+      paginaAtual,
+    });
 
-    const totalPages = Math.ceil(totalActions / limitNumber);
-
-    res.status(200).json({
-      actions,
-      totalPages,
-      currentPage: pageNumber,
+    response.status(200).json({
+      actions: acoes,
+      totalPages: totalDePaginas,
+      currentPage: paginaAtual,
     });
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    response.status(500).json({ error: (error as Error).message });
   }
 }
 
-export async function create(req, res) {
+export async function create(request: Request, response: Response) {
   try {
-    const response = await acaoRN.criarAcao(req.body);
-    res.status(201).json({ id: response });
+    const dados = request.body;
+
+    const criarAcao = new CriarAcao(acaoPrismaRepository);
+    const acao = await criarAcao.executar(dados);
+
+    response.status(201).json(acao);
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    response.status(400).json({ error: (error as Error).message });
   }
 }
 
-export async function update(req, res) {
+export async function update(request: Request, response: Response) {
   try {
-    const { id } = req.params;
-    res.status(201).json(await acaoRN.atualizarAcao(id, req.body));
+    const { id } = request.params;
+    response.status(201).json(await acaoRN.atualizarAcao(id, request.body));
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    response.status(400).json({ error: error.message });
   }
 }
 
-export async function buscarPorData(req, res) {
+export async function buscarPorData(request: Request, response: Response) {
   try {
-    const acao = await acaoRN.buscarPorData(req.params.data);
-    res.status(200).json(acao);
+    const acao = await acaoRN.buscarPorData(request.params.data);
+    response.status(200).json(acao);
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    response.status(400).json({ error: error.message });
   }
 }
 
-export async function buscarPorIntervaloData(req, res) {
-  console.log(req.params);
+export async function buscarPorIntervaloData(request: Request, response: Response) {
+  console.log(request.params);
   try {
-    const acao = await acaoRN.buscarPorIntervaloData(req.params.dataInicial, req.params.dataFinal);
-    res.status(200).json(acao);
+    const acao = await acaoRN.buscarPorIntervaloData(
+      request.params.dataInicial,
+      request.params.dataFinal
+    );
+    response.status(200).json(acao);
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    response.status(400).json({ error: error.message });
   }
 }
