d841b7bbb2af7da2650aa896deacc28b5aec23e3
viperluan <viperluan@gmail.com>
2024-09-05 18:05:06 -0300

    adiciona validação e propriedades para entidade ação

--- src/domain/acao/entity/Acao.ts
+++ src/domain/acao/entity/Acao.ts
@@ -1,4 +1,6 @@
 import { v4 as gerarUuid } from 'uuid';
+import { AcaoSituacao } from '../enum/AcaoSituacao';
+import { AcaoFormaRealizacao } from '../enum/AcaoFormaRealizacao';
 
 export type AcaoProps = {
   id: string;
@@ -11,27 +13,234 @@ export type AcaoProps = {
   local_acao: string;
   numero_organizadores_acao: number;
   receber_informacao_patrocinio: boolean;
-  situacao_acao: number;
+  situacao_acao: string;
   data_acao: Date;
   data_cadastro: Date;
   data_atualizacao: Date;
   id_categoria: string;
-  id_usuario_reponsavel: string;
-  id_usuario_alteracao: string | null;
+  id_usuario_responsavel: string;
+  id_usuario_alteracao: string;
+  categoria?: {
+    descricao: string;
+  };
+  usuario_responsavel?: {
+    nome: string;
+    email: string;
+  };
+  usuario_alteracao?: {
+    nome: string;
+    email: string;
+  };
+};
+
+type OmitirDadosNovaAcaoProps =
+  | 'id'
+  | 'data_cadastro'
+  | 'data_atualizacao'
+  | 'data_acao'
+  | 'receber_informacao_patrocinio'
+  | 'situacao_acao';
+
+type NovaAcaoProps = Omit<AcaoProps, OmitirDadosNovaAcaoProps> & {
+  data_acao: string;
 };
 
 export default class Acao {
   private constructor(readonly props: AcaoProps) {}
 
-  public static criarNovaAcao(novaAcao: Omit<AcaoProps, 'id' | 'id_usuario_alteracao'>) {
+  public static criarNovaAcao(acao: NovaAcaoProps) {
+    this.validacao(acao);
+
     return new Acao({
-      ...novaAcao,
+      ...acao,
       id: gerarUuid(),
-      id_usuario_alteracao: null,
+      id_usuario_alteracao: acao.id_usuario_responsavel,
+      receber_informacao_patrocinio: false,
+      situacao_acao: AcaoSituacao.AguardandoConfirmacao,
+      data_acao: new Date(acao.data_acao),
+      data_cadastro: new Date(),
+      data_atualizacao: new Date(),
     });
   }
 
   public static carregarAcaoExistente(props: AcaoProps) {
     return new Acao(props);
+  }
+
+  private static validacao(dadosParaValidar: NovaAcaoProps) {
+    const {
+      celular,
+      data_acao,
+      descricao_acao,
+      forma_realizacao_acao,
+      id_categoria,
+      id_usuario_responsavel,
+      link_organizador,
+      local_acao,
+      nome_organizador,
+      numero_organizadores_acao,
+      titulo_acao,
+    } = dadosParaValidar;
+
+    this.validarTituloAcao(titulo_acao);
+    this.validarCelular(celular);
+    this.validarDataAcao(data_acao);
+    this.validarDescricaoAcao(descricao_acao);
+    this.validarFormaRealizacaoAcao(forma_realizacao_acao);
+    this.validarIdCategoria(id_categoria);
+    this.validarIdUsuarioResponsavel(id_usuario_responsavel);
+    this.validarLinkOrganizador(link_organizador);
+    this.validarLocalAcao(local_acao);
+    this.validarNomeOrganizador(nome_organizador);
+    this.validarNumeroOrganizadoresAcao(numero_organizadores_acao);
+  }
+
+  private static validarTituloAcao(tituloAcao: string) {
+    if (!tituloAcao) throw new Error('A ação deve possuir um título.');
+  }
+
+  private static validarCelular(celular: string) {
+    if (!celular) throw new Error('A ação deve possuir um número de celular.');
+
+    if (celular.length !== 11) throw new Error('Número de celular inválido.');
+  }
+
+  private static validarDataAcao(dataAcao: string) {
+    if (!dataAcao) throw new Error('A ação deve possuir uma data de realização.');
+
+    const dataAtual = new Date();
+    const dataAcaoSolicitada = new Date(dataAcao);
+
+    if (isNaN(dataAcaoSolicitada.getTime())) throw new Error('Data de ação inválida.');
+
+    if (dataAcaoSolicitada <= dataAtual)
+      throw new Error('Data de ação deve ser posterior a data atual.');
+  }
+
+  private static validarDescricaoAcao(descricao: string) {
+    if (!descricao) throw new Error('A ação deve possuir uma descrição.');
+  }
+
+  private static validarFormaRealizacaoAcao(formaRealizacaoAcao: string) {
+    if (!formaRealizacaoAcao) throw new Error('A ação deve possuir uma forma de realização.');
+
+    const formaAcaoValida = Object.values(AcaoFormaRealizacao).includes(
+      formaRealizacaoAcao as AcaoFormaRealizacao
+    );
+
+    if (!formaAcaoValida) throw new Error('Forma de realização inválida.');
+  }
+
+  private static validarIdCategoria(idCategoria: string) {
+    if (!idCategoria) throw new Error('A ação deve possuir uma categoria informada.');
+  }
+
+  private static validarIdUsuarioResponsavel(idUsuarioResponsavel: string) {
+    if (!idUsuarioResponsavel) throw new Error('A ação deve possuir um usuário responsável.');
+  }
+
+  private static validarLinkOrganizador(linkOrganizador: string) {
+    if (!linkOrganizador) throw new Error('A ação deve possuir um link de organizador.');
+  }
+
+  private static validarLocalAcao(localAcao: string) {
+    if (!localAcao) throw new Error('A ação deve possuir um local de realização.');
+  }
+
+  private static validarNomeOrganizador(nomeOrganizador: string) {
+    if (!nomeOrganizador) throw new Error('A ação deve possuir um nome de organizador.');
+  }
+
+  private static validarNumeroOrganizadoresAcao(numeroOrganizadoresAcao: number) {
+    if (!numeroOrganizadoresAcao || numeroOrganizadoresAcao < 1)
+      throw new Error('A ação deve possuir o número aproximado de organizadores');
+  }
+
+  public get id(): string {
+    return this.props.id;
+  }
+
+  public get celular(): string {
+    return this.props.celular;
+  }
+
+  public get nome_organizador(): string {
+    return this.props.nome_organizador;
+  }
+
+  public get link_organizador(): string {
+    return this.props.link_organizador;
+  }
+
+  public get titulo_acao(): string {
+    return this.props.titulo_acao;
+  }
+
+  public get descricao_acao(): string {
+    return this.props.descricao_acao;
+  }
+
+  public get forma_realizacao_acao(): string {
+    return this.props.forma_realizacao_acao;
+  }
+
+  public get local_acao(): string {
+    return this.props.local_acao;
+  }
+
+  public get numero_organizadores_acao(): number {
+    return this.props.numero_organizadores_acao;
+  }
+
+  public get receber_informacao_patrocinio(): boolean {
+    return this.props.receber_informacao_patrocinio;
+  }
+
+  public get situacao_acao(): string {
+    return this.props.situacao_acao;
+  }
+
+  public get data_acao(): Date {
+    return this.props.data_acao;
+  }
+
+  public get data_cadastro(): Date {
+    return this.props.data_cadastro;
+  }
+
+  public get data_atualizacao(): Date {
+    return this.props.data_atualizacao;
+  }
+
+  public get id_categoria(): string {
+    return this.props.id_categoria;
+  }
+
+  public get id_usuario_responsavel(): string {
+    return this.props.id_usuario_responsavel;
+  }
+
+  public get id_usuario_alteracao(): string {
+    return this.props.id_usuario_alteracao;
+  }
+
+  public get categoria() {
+    return {
+      descricao: this.props.categoria?.descricao,
+    };
+  }
+
+  public get usuario_responsavel() {
+    return {
+      nome: this.props.usuario_responsavel?.nome,
+      email: this.props.usuario_responsavel?.email,
+    };
+  }
+
+  public get usuario_alteracao() {
+    return {
+      nome: this.props.usuario_alteracao?.nome,
+      email: this.props.usuario_alteracao?.email,
+    };
   }
 }
