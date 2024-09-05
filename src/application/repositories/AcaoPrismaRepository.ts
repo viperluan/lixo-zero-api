20640ff9de615d5412434b85fb2680e3012a5dbc
viperluan <viperluan@gmail.com>
2024-09-05 18:50:04 -0300

    implementa primeiros métodos para lidar com ações, falta ainda busca por data

---
+++ src/application/repositories/AcaoPrismaRepository.ts
@@ -0,0 +1,176 @@
+import { Prisma, PrismaClient } from '@prisma/client';
+import Acao from '../../domain/acao/entity/Acao';
+import IAcaoRepository from '../../domain/acao/repository/IAcaoRepository';
+
+export type FiltrosListarComPaginacaoType = {
+  id_categoria?: string;
+  id_usuario?: string;
+  id_usuario_responsavel?: string;
+  data_acao?: string;
+  search?: string;
+  situacao?: string;
+  forma_realizacao_acao?: string;
+};
+
+export default class AcaoPrismaRepository implements IAcaoRepository {
+  constructor(private readonly prisma: PrismaClient) {}
+
+  async buscarPorId(id: string): Promise<Acao | null> {
+    const acao = await this.prisma.acao.findFirst({ where: { id } });
+
+    if (!acao) return null;
+
+    return Acao.carregarAcaoExistente(acao);
+  }
+
+  async buscarPorTitulo(titulo: string): Promise<Acao | null> {
+    const acao = await this.prisma.acao.findFirst({ where: { titulo_acao: titulo } });
+
+    if (!acao) return null;
+
+    return Acao.carregarAcaoExistente(acao);
+  }
+
+  async buscarQuantidadeDeAcoes(): Promise<number> {
+    const quantidadeDeAcoes = await this.prisma.acao.count();
+
+    return quantidadeDeAcoes;
+  }
+
+  async listarComPaginacao(
+    filtros: FiltrosListarComPaginacaoType,
+    pagina = 1,
+    limiteAcoes = 10
+  ): Promise<Acao[] | null> {
+    const where: Prisma.AcaoWhereInput = {};
+
+    if (filtros.id_categoria) {
+      where.id_categoria = filtros.id_categoria;
+    }
+
+    if (filtros.id_usuario) {
+      where.id_usuario_responsavel = filtros.id_usuario;
+    }
+
+    if (filtros.data_acao) {
+      where.data_acao = new Date(filtros.data_acao);
+    }
+
+    if (filtros.search) {
+      where.OR = [
+        { titulo_acao: { contains: filtros.search, mode: 'insensitive' } },
+        { descricao_acao: { contains: filtros.search, mode: 'insensitive' } },
+        { nome_organizador: { contains: filtros.search, mode: 'insensitive' } },
+        { local_acao: { contains: filtros.search, mode: 'insensitive' } },
+      ];
+    }
+
+    if (filtros.situacao) {
+      where.situacao_acao = filtros.situacao;
+    }
+
+    if (filtros.forma_realizacao_acao) {
+      where.forma_realizacao_acao = filtros.forma_realizacao_acao;
+    }
+
+    const listaDeAcoes = await this.prisma.acao.findMany({
+      where,
+      include: {
+        categoria: {
+          select: {
+            descricao: true,
+          },
+        },
+        usuario_responsavel: {
+          select: {
+            nome: true,
+            email: true,
+          },
+        },
+        usuario_alteracao: {
+          select: {
+            nome: true,
+            email: true,
+          },
+        },
+      },
+      skip: (pagina - 1) * limiteAcoes,
+      take: limiteAcoes,
+    });
+
+    if (!listaDeAcoes) return null;
+
+    const acoes = listaDeAcoes.map((acao) => Acao.carregarAcaoExistente(acao));
+
+    return acoes;
+  }
+
+  async listarPorData(data: Date): Promise<Acao[] | null> {
+    throw new Error('Method not implemented.');
+  }
+
+  async listarPorIntervaloData(dataInicial: Date, dataFinal: Date): Promise<Acao[] | null> {
+    throw new Error('Method not implemented.');
+  }
+
+  async salvar({
+    celular,
+    data_acao,
+    data_atualizacao,
+    data_cadastro,
+    descricao_acao,
+    forma_realizacao_acao,
+    id,
+    id_categoria,
+    id_usuario_alteracao,
+    id_usuario_responsavel,
+    link_organizador,
+    local_acao,
+    nome_organizador,
+    numero_organizadores_acao,
+    receber_informacao_patrocinio,
+    situacao_acao,
+    titulo_acao,
+  }: Acao): Promise<void> {
+    const data = {
+      celular,
+      data_acao,
+      data_atualizacao,
+      data_cadastro,
+      descricao_acao,
+      forma_realizacao_acao,
+      id,
+      id_categoria,
+      id_usuario_alteracao,
+      id_usuario_responsavel,
+      link_organizador,
+      local_acao,
+      nome_organizador,
+      numero_organizadores_acao,
+      receber_informacao_patrocinio,
+      situacao_acao,
+      titulo_acao,
+    };
+
+    await this.prisma.acao.create({ data });
+  }
+
+  async atualizar(
+    id: string,
+    campos: Pick<Acao, 'situacao_acao' | 'id_usuario_alteracao'>
+  ): Promise<Acao> {
+    const acaoAtualizada = await this.prisma.acao.update({
+      where: { id },
+      data: {
+        situacao_acao: campos.situacao_acao,
+        id_usuario_alteracao: campos.id_usuario_alteracao,
+      },
+    });
+
+    return Acao.carregarAcaoExistente(acaoAtualizada);
+  }
+
+  async deletar(id: string): Promise<void> {
+    await this.prisma.acao.delete({ where: { id } });
+  }
+}
