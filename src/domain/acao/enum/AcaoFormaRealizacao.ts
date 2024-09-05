e2e88c1df44a64d072d3d5c0fd137ee6247228fc
viperluan <viperluan@gmail.com>
2024-09-05 18:05:26 -0300

    cria enuns para ação

---
+++ src/domain/acao/enum/AcaoFormaRealizacao.ts
@@ -0,0 +1,5 @@
+export enum AcaoFormaRealizacao {
+  Online = '0',
+  Presencial = '1',
+  Hibrida = '2',
+}
---
+++ src/domain/acao/enum/AcaoSituacao.ts
@@ -0,0 +1,5 @@
+export enum AcaoSituacao {
+  AguardandoConfirmacao = '0',
+  Confirmada = '1',
+  Cancelada = '2',
+}
