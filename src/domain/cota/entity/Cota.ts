7cc9ebec7a294675b6a20ca121993e00b3750925
viperluan <viperluan@gmail.com>
2024-09-08 20:44:00 -0300

    adiciona infos na entidade cota

--- src/domain/cota/entity/Cota.ts
+++ src/domain/cota/entity/Cota.ts
@@ -5,17 +5,43 @@ export type CotaProps = {
   descricao: string;
 };
 
+type OmitirDadosNovaCotaProps = 'id';
+
+type NovaCotaProps = Omit<CotaProps, OmitirDadosNovaCotaProps>;
+
 export default class Cota {
   private constructor(readonly props: CotaProps) {}
 
-  public static criarNovaCota(descricao: string) {
+  public static criarNovaCota(cota: NovaCotaProps) {
+    this.validacao(cota);
+
     return new Cota({
       id: gerarUuid(),
-      descricao,
+      descricao: cota.descricao,
     });
   }
 
   public static carregarCotaExistente(props: CotaProps) {
     return new Cota(props);
+  }
+
+  private static validacao({ descricao }: NovaCotaProps) {
+    this.validarDescricao(descricao);
+  }
+
+  private static validarDescricao(descricao: string) {
+    if (!descricao) throw new Error('A cota deve possuir uma descrição');
+
+    if (descricao.length > 100) {
+      throw new Error('A descrição não pode conter mais de 100 caracteres.');
+    }
+  }
+
+  public get id(): string {
+    return this.props.id;
+  }
+
+  public get descricao(): string {
+    return this.props.descricao;
   }
 }
