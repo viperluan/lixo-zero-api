d7191104ff1bbb248f8e0c80c2c59d078b308cdf
viperluan <viperluan@gmail.com>
2024-09-09 17:54:59 -0300

    cria repository de categoria para prisma

---
+++ src/application/repositories/CategoriaPrismaRepository.ts
@@ -0,0 +1,66 @@
+import { PrismaClient } from '@prisma/client';
+import Categoria from '../../domain/categoria/entity/Categoria';
+import ICategoriaRepository from '../../domain/categoria/repository/ICategoriaRepository';
+
+export default class CategoriaPrismaRepository implements ICategoriaRepository {
+  constructor(private readonly prisma: PrismaClient) {}
+
+  public async buscarPorId(id: string): Promise<Categoria | null> {
+    const categoria = await this.prisma.categoria.findFirst({ where: { id } });
+
+    if (!categoria) return null;
+
+    return Categoria.carregarCategoriaExistente(categoria);
+  }
+
+  public async buscarPorDescricao(descricao: string): Promise<Categoria | null> {
+    const categoria = await this.prisma.categoria.findFirst({ where: { descricao } });
+
+    if (!categoria) return null;
+
+    return Categoria.carregarCategoriaExistente(categoria);
+  }
+
+  public async buscarComPaginacao(
+    paginaAtual: number,
+    limiteDeCategoriasPorPagina: number
+  ): Promise<Categoria[] | null> {
+    const listaDeCategorias = await this.prisma.cota.findMany({
+      skip: (paginaAtual - 1) * limiteDeCategoriasPorPagina,
+      take: limiteDeCategoriasPorPagina,
+    });
+
+    const categorias = listaDeCategorias.map((categoria) =>
+      Categoria.carregarCategoriaExistente(categoria)
+    );
+
+    return categorias;
+  }
+
+  public async buscarQuantidadeCategorias(): Promise<number> {
+    const quantidadeCategorias = await this.prisma.categoria.count();
+
+    return quantidadeCategorias;
+  }
+
+  public async salvar({ id, descricao }: Categoria): Promise<void> {
+    const data = {
+      id,
+      descricao,
+    };
+
+    await this.prisma.cota.create({ data });
+  }
+
+  public async atualizar({ id, descricao }: Categoria): Promise<void> {
+    const data = {
+      descricao,
+    };
+
+    await this.prisma.cota.update({ where: { id }, data });
+  }
+
+  public async deletar(id: string): Promise<void> {
+    await this.prisma.categoria.delete({ where: { id } });
+  }
+}
