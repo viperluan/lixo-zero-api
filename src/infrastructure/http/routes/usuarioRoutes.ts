454fe95f1b1ec921f11b623514e3e8a22e5a2c52
viperluan <viperluan@gmail.com>
2024-09-19 18:04:36 -0300

    adiciona middlewares para rotas de usuário

--- src/infrastructure/http/routes/usuarioRoutes.ts
+++ src/infrastructure/http/routes/usuarioRoutes.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 
 import * as usuarioController from '../controllers/UsuarioController';
+import AutenticacaoMiddleware from '../middlewares/AutenticacaoMiddleware';
+import AdminMiddleware from '../middlewares/AdminMiddleware';
 
 const usuarioRouter = Router();
 
 usuarioRouter.post('/', usuarioController.criar);
-usuarioRouter.get('/', usuarioController.buscarTodos);
-usuarioRouter.delete('/:id', usuarioController.remover);
+usuarioRouter.get('/', AutenticacaoMiddleware, AdminMiddleware, usuarioController.buscarTodos);
+usuarioRouter.delete('/:id', AutenticacaoMiddleware, usuarioController.remover);
 usuarioRouter.post('/autenticar', usuarioController.autenticar);
 
 export default usuarioRouter;
