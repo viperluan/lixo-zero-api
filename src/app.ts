bf988171619dcd19167d254acc789f58f6795523
viperluan <viperluan@gmail.com>
2026-08-23 01:35:25 -0300

    chore: adiciona helmet para mais segurança

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import helmet from 'helmet';
 import routes from './infrastructure/http/routes';
 import { obterOpcoesCors } from './infrastructure/http/config/cors';
 import { criarRateLimitGlobal } from './infrastructure/http/config/rateLimit';
@@ -8,8 +9,9 @@ const app = express();
 
 app.set('trust proxy', 1);
 
+app.use(helmet({ contentSecurityPolicy: false }));
 app.use(cors(obterOpcoesCors()));
-app.use(express.json());
+app.use(express.json({ limit: '500kb' }));
 app.use(criarRateLimitGlobal());
 
 app.use(routes);
